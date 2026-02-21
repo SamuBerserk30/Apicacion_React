0468c0257dce8d934e9c7bf4361faad6944a4023
SamuBerserk30 <samuelsotocdn@gmail.com>
2026-02-20 19:58:40 -0500

    feat: agregar stock a productos y mostrarlo en la UI

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 
 import styles from './ProductCard.module.css';
 
-function ProductCard({ name, category, price, image, description }) {
+function ProductCard({ name, category, price, stock, image, description }) {
     const [likes, setLikes] = useState(0);
     const [isLiked, setIsLiked] = useState(false);
 
@@ -21,6 +21,7 @@ function ProductCard({ name, category, price, image, description }) {
         <div className={styles.productInfo}>
            <span className={styles.productCategory}>{category}</span>
            <h3 className={styles.productName}>{name}</h3>
+           <p className={styles.productStock}>Stock: {stock}</p>
            <p className={styles.productDescription}>{description}</p>
            <div className={styles.productFooter}>
                 <span className={styles.productPrice}>${price.toFixed(2)}</span>
--- src/data/Product.js
+++ src/data/Product.js
@@ -3,7 +3,8 @@ export const products = [
     id: 1,
     name: 'Laptop HP Pavilion',
     category: 'Laptops',
-    price: 2499.99,
+    price: 2500000,
+    stock: 5,
     image: 'src/assets/compu.jpg',
     description: 'Laptop potente con procesador Intel Core i5, 16GB de RAM y almacenamiento SSD de 512GB. Ideal para tareas exigentes y juegos.'
   },
@@ -12,6 +13,7 @@ export const products = [
     name: 'Smartphone Samsung Galaxy S21',
     category: 'Smartphones',
     price: 3500000,
+    stock: 5,
     image: 'src/assets/celuco.webp',
     description: 'Smartphone Samsung Galaxy S21 con pantalla AMOLED de 6.2 pulgadas, cámara de alta resolución y batería de larga duración. Perfecto para capturar momentos y disfrutar de contenido multimedia.'  
  },
@@ -19,7 +21,8 @@ export const products = [
     id: 3,
     name: 'Auriculares Sony WH-1000XM4',
     category: 'Audio',
-    price: 1200000,
+    price: 120000,
+    stock: 5,
     image: 'src/assets/auriculares.jpg',
     description: 'Audifonos Sony WH-1000XM4 con cancelación de ruido, sonido de alta calidad y batería de larga duración. Perfectos para música y llamadas.'
  },
@@ -27,7 +30,8 @@ export const products = [
     id: 4,
     name: 'Smartwatch Apple Watch Series 6',
     category: 'Wearables',
-    price: 2500000,
+    price: 1000000,
+    stock: 5,
     image: 'src/assets/reloj.jpg',
     description: 'Smartwatch Apple Watch Series 6 con monitoreo de salud, GPS integrado y resistencia al agua. Ideal para mantenerse conectado y activo durante todo el día.'
  }
