0868a4b90bdeced5cc3d4ffdc3548cb3cb620b07
SamuBerserk30 <samuelsotocdn@gmail.com>
2026-02-20 21:01:58 -0500

    feat: agregar producto al estado de productos

--- src/Pages/ProductList.jsx
+++ src/Pages/ProductList.jsx
@@ -2,9 +2,20 @@ import { useState } from "react";
 import { products } from '../data/Product';
 import ProductCard from '../components/ProductCard';
 import styles from './ProductList.module.css';
+import ProductForm from "../components/ProductForm";
 
 function ProductList() {
     const [productsState, setProductsState] = useState(products);
+
+const handleAddProduct = (product) => {
+  setProductsState((prev) => {
+    const maxId = prev.reduce((acc, item) => Math.max(acc, item.id), 0);
+    const nextId = maxId + 1;
+
+    return [...prev, { ...product, id: nextId }];
+  });
+};
+
     return (
         <div className={styles.container}>
             <header className={styles.header}>
@@ -13,7 +24,7 @@ function ProductList() {
                     Encuentra los mejores productos de tecnologia pra tu setup
                 </p>
             </header>
-
+            <ProductForm onSubmit={handleAddProduct} />
             <div className={styles.grid}>
                 {productsState.map((product) => (
                     <ProductCard
