2193e06aeba4e85892e64be0df7ec3ee7b19a922
SamuBerserk30 <samuelsotocdn@gmail.com>
2026-02-22 18:40:55 -0500

    editando productos

--- src/data/Product.js
+++ src/data/Product.js
@@ -3,7 +3,7 @@ export const products = [
     id: 1,
     name: 'Laptop HP Pavilion',
     category: 'Laptops',
-    price: 2500000,
+    price: 500,
     stock: 5,
     image: 'src/assets/compu.jpg',
     description: 'Laptop potente con procesador Intel Core i5, 16GB de RAM y almacenamiento SSD de 512GB. Ideal para tareas exigentes y juegos.'
@@ -12,7 +12,7 @@ export const products = [
     id: 2,
     name: 'Smartphone Samsung Galaxy S21',
     category: 'Smartphones',
-    price: 3500000,
+    price: 800,
     stock: 5,
     image: 'src/assets/celuco.webp',
     description: 'Smartphone Samsung Galaxy S21 con pantalla AMOLED de 6.2 pulgadas, cámara de alta resolución y batería de larga duración. Perfecto para capturar momentos y disfrutar de contenido multimedia.'  
@@ -21,7 +21,7 @@ export const products = [
     id: 3,
     name: 'Auriculares Sony WH-1000XM4',
     category: 'Audio',
-    price: 120000,
+    price: 120,
     stock: 5,
     image: 'src/assets/auriculares.jpg',
     description: 'Audifonos Sony WH-1000XM4 con cancelación de ruido, sonido de alta calidad y batería de larga duración. Perfectos para música y llamadas.'
@@ -30,7 +30,7 @@ export const products = [
     id: 4,
     name: 'Smartwatch Apple Watch Series 6',
     category: 'Wearables',
-    price: 1000000,
+    price: 200,
     stock: 5,
     image: 'src/assets/reloj.jpg',
     description: 'Smartwatch Apple Watch Series 6 con monitoreo de salud, GPS integrado y resistencia al agua. Ideal para mantenerse conectado y activo durante todo el día.'
